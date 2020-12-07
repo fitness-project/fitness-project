fa4df2c9caac7a2dc5883ababcf992637682f1ad
Zihao <zihaozhang1@gmail.com>
2020-12-07 18:53:22 -0500

    commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,23 @@
+<<<<<<< HEAD
 
   .then(function(data) {
     console.log(data);
   });
+=======
+var quoteButton = document.querySelector('#btn');
+
+
+var getQuote = function () {
+
+    fetch("https://type.fit/api/quotes").then(function(response) {
+    return response.json();
+    })
+    .then(function(data) {
+        console.log(data);
+    });
+    }
+
+quoteButton.addEventListener('click', getQuote);
+  
+getQuote();
+>>>>>>> 2d373f8734590f72b36ae4be4726fd1e3c2c6263
